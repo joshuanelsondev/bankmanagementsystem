e92a7ca1e6e953ef90d31e9f9361bc4c757d9a6d
joshuanelsondev <joshuanelsondev@gmail.com>
2025-03-29 17:48:26 -0400

    feat: create BankAccount class and its corresponding variables and methods

--- src/main/java/joshuanelsondev/bankmanagementsystem/BankManagementSystem.java
+++ src/main/java/joshuanelsondev/bankmanagementsystem/BankManagementSystem.java
@@ -1,4 +1,7 @@
 package joshuanelsondev.bankmanagementsystem;
 
 public class BankManagementSystem {
+    public static void main(String[] args) {
+        System.out.println("Bank Management System");
+    }
 }
